67c845e8e533dfc68a9208149aa862a8a29c920b
Fabio Chicout <fchicout@gmail.com>
2019-07-11 00:33:58 -0300

    thermistor code added. tests on real circuit not done yet

--- sports_tracker.ino
+++ sports_tracker.ino
@@ -3,11 +3,19 @@
 
 #include <SoftwareSerial.h>
 
-File logData;
+// Pinning
+int thermistorPin = A0; // Thermistor Pin
 
-void setup(){
+// Global Variables
+File logData; // Log File Pointer
+int Vo; // Thermistor voltage read
+float R1 = 10000; // Fix with own thermistor resistance;
+float logR2, R2, T;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+
+void setup() {
   Serial.begin(9600); // Serial Monitor!
-  while(!Serial){
+  while (!Serial) {
     ;
   }
 
@@ -19,14 +27,20 @@ void setup(){
   }
   Serial.println("initialization done.");
 
-  
+
 }
 
-void loop(){
+void loop() {
   logData = SD.open("logdata.csv", FILE_WRITE);
 
-  if(logData) {
-    logData.print("bodyTemperature");
+  Vo = analogRead(thermistorPin); 
+  R2 = R1 * (1023.0 / (float)Vo - 1.0); 
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
+
+  if (logData) {
+    logData.print(T);
+    logData.print(",");
     logData.println("GPS DATA");
     logData.close();
   }
